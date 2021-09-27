7ab8468b936bf7170f3a0f69a123e76417c4d262
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-09-26 22:13:11 -0500

    include link to live site; handle errors more gracefully

--- app/src/components/board/BoardContainer.tsx
+++ app/src/components/board/BoardContainer.tsx
@@ -1,5 +1,5 @@
-import axios, { AxiosResponse } from "axios";
-import React from "react";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import React, { useEffect, useState } from "react";
 import { Board } from "./Board";
 import { useBoard } from "./use-board";
 import { useSelection } from "./use-selection";
@@ -10,24 +10,47 @@ const solveURL =
 export const BoardContainer: React.FunctionComponent = () => {
     const [currentSelection, setCurrentSelection] = useSelection();
     const { board, setBoard } = useBoard(currentSelection);
+    const [error, setError] = useState("");
+    useEffect(() => {
+        if (error !== "") {
+            const timer = setTimeout(() => setError(""), 5000);
+            return () => clearTimeout(timer);
+        }
+    }, [error]);
 
     const solve = async () => {
         const data = board.map(r => [...r.map(n => n.value)]);
-        const response: AxiosResponse<number[][]> = await axios.post(
-            solveURL,
-            data,
-        );
-        setBoard(response.data);
+        try {
+            const response: AxiosResponse<number[][]> = await axios.post(
+                solveURL,
+                data,
+            );
+            setBoard(response.data);
+        } catch (err) {
+            let maybeAxiosErr = err as Error | AxiosError;
+            if (!axios.isAxiosError(maybeAxiosErr)) {
+                console.log(maybeAxiosErr.message);
+                return;
+            }
+            setError(maybeAxiosErr.response?.data?.detail ?? "");
+        }
     };
 
     return (
         <div className="p-16">
-            <button
-                className=" mb-2 py-1 px-2 w-32 rounded-md shadow-md text-gray-50 bg-blue-500 hover:bg-blue-600"
-                onClick={solve}
-            >
-                Solve
-            </button>
+            <div className="flex flex-row justify-between mb-2">
+                <button
+                    className="py-1 px-2 w-32 rounded-md shadow-md text-gray-50 bg-blue-500 hover:bg-blue-600"
+                    onClick={solve}
+                >
+                    Solve
+                </button>
+                {error !== "" && (
+                    <div className="py-1 px-2 bg-red-200 rounded-md">
+                        {error}
+                    </div>
+                )}
+            </div>
             <Board
                 board={board}
                 currentSelection={currentSelection}
